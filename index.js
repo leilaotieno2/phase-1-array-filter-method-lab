3693a7ae173a6754592a8f192924370a61aa46fc
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-06 18:42:52 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    const lengthOfLetters = letters.length;
+    return drivers.filter(driver => driver.slice(0, lengthOfLetters) === letters);
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+  function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    const lengthOfLetters = letters.length;
+    return drivers.filter(driver => driver.slice(0, lengthOfLetters) === letters);
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+  }
+      
